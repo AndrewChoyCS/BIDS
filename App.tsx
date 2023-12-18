df3f95e16b02d7f723c4c4f41864ea9bb55a2747
Sophia Prakasa <sophiaprakasa@berkeley.edu>
2023-12-18 01:29:05 -0800

    hello

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import LandingPage from './pages/LandingPage'; // Import your LandingPage component
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainStack = () => (
+  <Stack.Navigator initialRouteName="LandingPage">
+    <Stack.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }}/>
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LandingPage">
-        <Stack.Screen name="LandingPage" component={LandingPage} />
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="MainStack">
+        <Drawer.Screen name="MainStack" component={MainStack} />
+        {/* Add additional screens as needed */}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
